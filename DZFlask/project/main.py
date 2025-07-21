774e24a48cc172b857410d7de87ce4863b1e988c
Greenya <Greenya992@gmail.com>
2025-07-21 15:49:24 +0300

    Update main.py

--- DZFlask/project/main.py
+++ DZFlask/project/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/table")
+def table():
+    return render_template("table.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
